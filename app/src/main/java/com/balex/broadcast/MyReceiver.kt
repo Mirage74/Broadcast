e30b3bf8ddf6d6d2d5eb12e8356fc1b62fe0bfd6
balex <balexvicx@gmail.com>
2024-03-31 23:53:34 +0200

    own Receiver

--- app/src/main/java/com/balex/broadcast/MyReceiver.kt
+++ app/src/main/java/com/balex/broadcast/MyReceiver.kt
@@ -9,17 +9,27 @@ class MyReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         when (intent?.action) {
+            ACTION_CLICKED -> {
+                val count = intent.getIntExtra(EXTRA_COUNT, 0)
+                Toast.makeText(context, "Clicked $count", Toast.LENGTH_SHORT).show()
+            }
             Intent.ACTION_BATTERY_LOW -> {
-                Toast.makeText(context, "Battery low", Toast.LENGTH_LONG).show()
+                Toast.makeText(context, "Battery low", Toast.LENGTH_SHORT).show()
             }
             Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
                 val turnedOn = intent.getBooleanExtra("state", false)
                 Toast.makeText(
                     context,
                     "Airplane mode changed. Turned on: $turnedOn",
-                    Toast.LENGTH_LONG
+                    Toast.LENGTH_SHORT
                 ).show()
             }
         }
+    }
+
+    companion object {
+
+        const val ACTION_CLICKED = "clicked"
+        const val EXTRA_COUNT = "count"
     }
 }
