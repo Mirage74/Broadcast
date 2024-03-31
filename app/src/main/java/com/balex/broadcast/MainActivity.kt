6847522c2611981a3288f54582b661f258937cc1
balex <balexvicx@gmail.com>
2024-04-01 00:45:42 +0200

    receiver must be in activity (frag), when needed refresh view

--- app/src/main/java/com/balex/broadcast/MainActivity.kt
+++ app/src/main/java/com/balex/broadcast/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.balex.broadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class MainActivity : AppCompatActivity() {
 
-    private val receiver = MyReceiver()
+    private lateinit var progressBar: ProgressBar
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == "loaded") {
+                val percent = intent.getIntExtra("percent", 0)
+                progressBar.progress = percent
+            } else if (intent?.action == MyReceiver.ACTION_CLICKED) {
+                val count = intent.getIntExtra(MyReceiver.EXTRA_COUNT, 0)
+                Toast.makeText(context, "Clicked $count", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    //private val receiver = MyReceiver()
 
     private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progressBar = findViewById(R.id.progressBar)
         findViewById<Button>(R.id.button).setOnClickListener {
             Intent(MyReceiver.ACTION_CLICKED).apply {
                 putExtra(MyReceiver.EXTRA_COUNT, count++)
@@ -22,11 +44,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
         val intentFilter = IntentFilter().apply {
-            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-            addAction(Intent.ACTION_BATTERY_LOW)
+            addAction("loaded")
             addAction(MyReceiver.ACTION_CLICKED)
         }
         registerReceiver(receiver, intentFilter, RECEIVER_EXPORTED)
+        Intent(this, MyService::class.java).apply {
+            startService(this)
+        }
     }
 
     override fun onDestroy() {
@@ -34,3 +58,5 @@ class MainActivity : AppCompatActivity() {
         unregisterReceiver(receiver)
     }
 }
+
+
